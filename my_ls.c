97b21ca1f9aeabc1a3a15c283c2d08c29d44b913
xadicha <246abdurashidova123@gmail.com>
2024-03-08 01:10:32 +0500

    -a option added

--- my_ls.c
+++ my_ls.c
@@ -28,10 +28,15 @@ int my_strcmp(char *str1, char *str2)
 
 // int my_length(ls_listnode *head)
 // {
+//     ls_listnode* header = head;
 //     int count = 0;
-//     while (head != NULL)
+//     while (header != NULL)
 //     {
+//         count++;
+//         header = header->next;
 //     }
+
+//     return count;
 // }
 
 void my_linked_list_swap(ls_listnode *str1, ls_listnode *str2)
@@ -108,7 +113,7 @@ void newline_node(ls_listnode **head, char *file)
     }
 }
 
-void my_ls(ls_listnode **head)
+void my_ls(ls_listnode **head, int include_hidden)
 {
     DIR *direct;
     direct = opendir(".");
@@ -117,7 +122,11 @@ void my_ls(ls_listnode **head)
 
     while ((data = readdir(direct)) != NULL)
     {
-        newline_node(head, data->d_name);
+       if (include_hidden || data->d_name[0] != '.' || strcmp(data->d_name, ".") == 0 || strcmp(data->d_name, ".."))
+       {
+         newline_node(head, data->d_name);
+       }
+       
     }
     print_linked_list(*head);
 
@@ -127,14 +136,18 @@ void my_ls(ls_listnode **head)
 int main(int argc, char **argv)
 {
     struct ls_listnode *head = NULL;
-
+    int include_hidden = 0;
     for (int i = 0; i < argc; i++)
     {
         if (my_strcmp(argv[i], "-t") == 0)
         {
             sort_linked_list(head);
         }
+        else if(my_strcmp(argv[i], "-a") == 0)
+        {
+            include_hidden = 1;
+        }
     }
-    my_ls(&head);
+    my_ls(&head, include_hidden);
     return 0;
 }
